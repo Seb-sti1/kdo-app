8bcc6929549e46e544bb6928cb2f49748c813cf8
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2024-12-19 21:45:11 +0100

    feat: add footer

---
+++ src/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import './style/footer.scss'
+
+const Footer: React.FC = () => {
+    return <footer>
+        Developed by <a href="https://kerbourch.fr/" target="_blank">Seb-sti1</a>.
+        See the <a href="https://github.com/Seb-sti1/kdo-app" target="_blank">source code</a> on Github.
+    </footer>
+}
+
+export default Footer;
